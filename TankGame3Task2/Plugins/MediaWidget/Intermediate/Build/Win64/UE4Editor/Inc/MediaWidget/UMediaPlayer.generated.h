445ea247deaae464e29c01ddcd7724e5b2e9df00
bogguser <glinbb@gmail.com>
2021-11-14 18:28:30 +0300

    Glinb

---
+++ TankGame3Task2/Plugins/MediaWidget/Intermediate/Build/Win64/UE4Editor/Inc/MediaWidget/UMediaPlayer.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MEDIAWIDGET_UMediaPlayer_generated_h
+#error "UMediaPlayer.generated.h already included, missing '#pragma once' in UMediaPlayer.h"
+#endif
+#define MEDIAWIDGET_UMediaPlayer_generated_h
+
+#define TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_SPARSE_DATA
+#define TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_RPC_WRAPPERS
+#define TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_RPC_WRAPPERS_NO_PURE_DECLS
+#define TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUUMediaPlayer(); \
+	friend struct Z_Construct_UClass_UUMediaPlayer_Statics; \
+public: \
+	DECLARE_CLASS(UUMediaPlayer, UUserWidget, COMPILED_IN_FLAGS(CLASS_Abstract), CASTCLASS_None, TEXT("/Script/MediaWidget"), NO_API) \
+	DECLARE_SERIALIZER(UUMediaPlayer)
+
+
+#define TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_INCLASS \
+private: \
+	static void StaticRegisterNativesUUMediaPlayer(); \
+	friend struct Z_Construct_UClass_UUMediaPlayer_Statics; \
+public: \
+	DECLARE_CLASS(UUMediaPlayer, UUserWidget, COMPILED_IN_FLAGS(CLASS_Abstract), CASTCLASS_None, TEXT("/Script/MediaWidget"), NO_API) \
+	DECLARE_SERIALIZER(UUMediaPlayer)
+
+
+#define TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UUMediaPlayer(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UUMediaPlayer) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UUMediaPlayer); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UUMediaPlayer); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UUMediaPlayer(UUMediaPlayer&&); \
+	NO_API UUMediaPlayer(const UUMediaPlayer&); \
+public:
+
+
+#define TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UUMediaPlayer(UUMediaPlayer&&); \
+	NO_API UUMediaPlayer(const UUMediaPlayer&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UUMediaPlayer); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UUMediaPlayer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UUMediaPlayer)
+
+
+#define TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_PRIVATE_PROPERTY_OFFSET
+#define TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_22_PROLOG
+#define TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_PRIVATE_PROPERTY_OFFSET \
+	TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_SPARSE_DATA \
+	TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_RPC_WRAPPERS \
+	TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_INCLASS \
+	TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_PRIVATE_PROPERTY_OFFSET \
+	TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_SPARSE_DATA \
+	TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_RPC_WRAPPERS_NO_PURE_DECLS \
+	TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_INCLASS_NO_PURE_DECLS \
+	TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h_25_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> MEDIAWIDGET_API UClass* StaticClass<class UUMediaPlayer>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TankGame_Plugins_MediaWidget_Source_MediaWidget_Public_UMediaPlayer_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
