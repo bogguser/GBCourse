da9c414607b14c025f0a76e40d57250e63d303f1
bogguser <glinbb@gmail.com>
2021-12-06 14:36:56 +0300

    Task7

---
+++ TankGame3Task7/Source/TankGame/Player/TankPlayerController.cpp
@@ -0,0 +1,36 @@
+#include "TankPlayerController.h"
+
+#include "Components/WidgetInteractionComponent.h"
+
+ATankPlayerController::ATankPlayerController(const FObjectInitializer& Obj) : Super(Obj)
+{
+	bShowMouseCursor = false;
+	bEnableClickEvents = true;
+}
+
+void ATankPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	InputComponent->BindKey(EKeys::LeftMouseButton, IE_Released, this, &ATankPlayerController::OnLeftMouseButtonUp);
+}
+
+FVector ATankPlayerController::GetWorldMousePosition() const
+{
+	FVector MousePosition, MouseDirection;
+	DeprojectMousePositionToWorld(MousePosition,MouseDirection);
+	
+	return MousePosition;
+}
+
+void ATankPlayerController::OnLeftMouseButtonDown()
+{
+		
+}
+
+void ATankPlayerController::OnLeftMouseButtonUp()
+{
+	
+		OnLeftMouseButtonUpEvent.Broadcast();
+	
+}
+
